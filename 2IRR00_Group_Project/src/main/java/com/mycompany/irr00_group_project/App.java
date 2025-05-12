6f59b5090aee6fa7bd7abff4415c1c0d1ef77406
techinesis <efecoach0@gmail.com>
2025-05-12 17:08:24 +0200

    New Test

--- 2IRR00_Group_Project/src/main/java/com/mycompany/irr00_group_project/App.java
+++ 2IRR00_Group_Project/src/main/java/com/mycompany/irr00_group_project/App.java
@@ -7,6 +7,6 @@ package com.mycompany.irr00_group_project;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! TESTING TESTING");
+        System.out.println("Hello World! TESTING for VSCode pls work");
     }
 }
