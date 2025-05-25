08ce230ef9cc1b3223e8f176ea38a195def998a5
stefkrit27 <90135928+stefkrit27@users.noreply.github.com>
2025-05-25 15:45:27 +0200

    Added CodeEditor and ConsoleOutput components. Also made the game screen layout.

--- 2IRR00_Group_Project/src/main/java/com/mycompany/irr00_group_project/view/screen/GameScreen.java
+++ 2IRR00_Group_Project/src/main/java/com/mycompany/irr00_group_project/view/screen/GameScreen.java
@@ -3,5 +3,15 @@ package com.mycompany.irr00_group_project.view.screen;
 /**
  * .
  */
-public class GameScreen {
+public class GameScreen extends AbstractScreen{
+
+    @Override
+    protected String getFxmlPath() {
+        return "/com/mycompany/irr00_group_project/view/screen/GameScreen.fxml";
+    }
+
+    @Override
+    protected String getCssPath() {
+        return "/com/mycompany/irr00_group_project/assets/css/gameStyle.css";
+    }
 }
