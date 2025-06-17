197baa8c35851c58fb52cadc0fe09fa93c2f2271
mitkozh <mitko.zh@gmail.com>
2025-06-17 12:36:28 +0200

    Removed unnecessary assertEquals in testCompleteLevelAndSave_UnlocksNextLevel

--- 2IRR00_Group_Project/src/test/java/com/mycompany/irr00_group_project/service/core/LevelServiceTest.java
+++ 2IRR00_Group_Project/src/test/java/com/mycompany/irr00_group_project/service/core/LevelServiceTest.java
@@ -50,7 +50,6 @@ public class LevelServiceTest {
         levelService.completeLevelAndSave(newData);
         LevelDTO nextLevel = levelService.getLevelProgress(2);
         assertTrue(nextLevel.isUnlocked());
-        assertEquals(0, nextLevel.getStars());
     }
 
     @Test
