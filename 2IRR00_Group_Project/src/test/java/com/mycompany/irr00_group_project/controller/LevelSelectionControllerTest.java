f834f87ad9e702a82e3c654eebcd9cf3ac78c295
mitkozh <mitko.zh@gmail.com>
2025-06-18 14:02:29 +0200

    Deleted empty controller tests.

--- 2IRR00_Group_Project/src/test/java/com/mycompany/irr00_group_project/controller/LevelSelectionControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.irr00_group_project.controller;
-
-/**
- * JUnit test class for LevelSelectionController.
- */
-public class LevelSelectionControllerTest {
-}
--- 2IRR00_Group_Project/src/test/java/com/mycompany/irr00_group_project/controller/components/GameGridControllerTest.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.irr00_group_project.controller.components;
-
-/**
- * JUnit test class for GameGridController.
- */
-public class GameGridControllerTest {
-    
-}
